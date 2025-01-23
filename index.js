3ef6e299dbb0c6b152f7fcece671769a8f6aee0a
Hari Mohan <mohanhari061@gmail.com>
2025-01-24 00:22:15 +0530

    check after months

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const githubAPI={
+    "login": "mohanhari061",
+    "id": 115946069,
+    "node_id": "U_kgDOBukyVQ",
+    "avatar_url": "https://avatars.githubusercontent.com/u/115946069?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/mohanhari061",
+    "html_url": "https://github.com/mohanhari061",
+    "followers_url": "https://api.github.com/users/mohanhari061/followers",
+    "following_url": "https://api.github.com/users/mohanhari061/following{/other_user}",
+    "gists_url": "https://api.github.com/users/mohanhari061/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/mohanhari061/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/mohanhari061/subscriptions",
+    "organizations_url": "https://api.github.com/users/mohanhari061/orgs",
+    "repos_url": "https://api.github.com/users/mohanhari061/repos",
+    "events_url": "https://api.github.com/users/mohanhari061/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/mohanhari061/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 12,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 2,
+    "created_at": "2022-10-16T17:39:26Z",
+    "updated_at": "2024-08-02T13:33:04Z"
+}
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -15,7 +50,10 @@ app.get("/login", (req, res) => {
 app.get("/youtube", (req, res) => {
   res.send("<h2>Youtube!</h2>");
 });
+app.get("/github", (req, res) => {
+  res.json(githubAPI);
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port http://localhost:${port}/`);
 });
